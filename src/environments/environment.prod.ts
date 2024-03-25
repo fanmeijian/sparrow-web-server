34357ee0eb8d503749b8657085ab89324da35733
Sword <fanmeijian@gmail.com>
2024-03-25 10:43:24 +0800

    add env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  orgApiBase: 'https://api.linkair-tech.cn/org-service',
+  ruleApiBase: 'https://api.linkair-tech.cn/rule-service',
+  keycloakUrl: 'https://keycloak.sportunione.cn',
+  realm: 'liyun-prd',
 };
