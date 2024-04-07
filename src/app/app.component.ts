3fb7e039a04d84a7d68fb6b96d4331a56397efa5
Sword <fanmeijian@gmail.com>
2024-04-07 20:12:51 +0800

    update for temp publish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,15 @@ import Connect from './model/Connect';
 import Stage from './model/Stage';
 import TextElm from './model/TextElm';
 import { KeycloakService } from 'keycloak-angular';
-import { AuditlogService, EmployeeService, RestApiServiceService } from '@sparrowmini/org-api';
+import {
+  AuditlogService,
+  EmployeeService,
+  OrganizationService,
+  RestApiServiceService,
+  RoleService,
+} from '@sparrowmini/org-api';
 import { HttpClient } from '@angular/common/http';
+import { combineLatest, map, switchMap, zip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +28,7 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements AfterViewInit {
   @ViewChild('stage') stage!: ElementRef<any>;
   curUser: any;
+  organizationRoleId: any;
 
   constructor(
     private keycloakService: KeycloakService,
@@ -28,68 +36,55 @@ export class AppComponent implements AfterViewInit {
     private http: HttpClient,
     private rest: RestApiServiceService,
     private logService: AuditlogService,
+    private orgService: OrganizationService,
+    private roleService: RoleService
   ) {
     this.keycloakService.loadUserProfile().then((res) => {
       this.curUser = res;
       this.employeeService
         .employeeByUsername(res.username)
         .subscribe((a: any) => {
-          this.employeeService.employeeRoles(a.id).subscribe((b) => {
-            this.curUser.roles = b;
-          });
+          this.employeeService
+            .employeeRoles(a.id)
+            .pipe(
+              switchMap((m) =>
+                zip(
+                  ...m.map((r) => {
+                    const $org = this.orgService.org(
+                      r?.id?.organizationRoleId.organizationId
+                    );
+                    const $role = this.roleService.role(
+                      r.id.organizationRoleId.roleId
+                    );
+                    return combineLatest($org, $role).pipe(
+                      map((p: any) =>
+                        Object.assign(r, { org: p[0], role: p[1] })
+                      )
+                    );
+                  })
+                )
+              )
+            )
+            .subscribe((b) => {
+              // console.log(b);
+              this.curUser.roles = b;
+              let orgRoleId = JSON.parse(
+                sessionStorage.getItem('organizationId')
+              );
+              this.organizationRoleId = b.filter(
+                (f) =>
+                  f.id.organizationRoleId.organizationId ===
+                    orgRoleId.organizationId &&
+                  f.id.organizationRoleId.roleId === orgRoleId.roleId
+              )[0].id.organizationRoleId;
+
+              // console.log(b[0].id.organizationRoleId);
+            });
         });
     });
-
   }
-  ngAfterViewInit(): void {
-
-
-    // console.log(this.stage);
-    // // 初始化一个800 * 700的舞台
-    // let s2 = new Stage(this.stage.nativeElement);
-    // // 在 (50, 50)的位置初始化一个200 * 50的文字板
-    // let t1 = new TextElm({
-    //   text: 'hello 解决1',
-    //   x: 50,
-    //   y: 50,
-    //   w: 200,
-    //   h: 50,
-    //   color: 'blue',
-    //   parent: s2,
-    // });
-    // s2.add(t1);
-    // // 在 (300, 300)的位置初始化一个200 * 50的文字板
-    // let t2 = new TextElm({
-    //   text: 'world 哈哈2',
-    //   x: 300,
-    //   y: 400,
-    //   w: 200,
-    //   h: 50,
-    //   color: 'blue',
-    //   parent: s2,
-    // });
-    // s2.add(t2);
-    // let t3 = new TextElm({
-    //   text: '多连线3',
-    //   x: 300,
-    //   y: 100,
-    //   w: 200,
-    //   h: 50,
-    //   color: 'blue',
-    //   parent: s2,
-    // });
-    // s2.add(t3);
-    // // 在(10, 20)和(50， 20)的位置初始化一个连线箭头**这个连线方式要优化**，自行将箭头两端拖拽到需要连接的元素上，之后元素移动会自动更新连线
-    // let connect = new Connect({ x: 10, y: 150 }, { x: 150, y: 150 });
-    // s2.add(connect);
-    // let connect2 = new Connect({ x: 10, y: 250 }, { x: 150, y: 250 });
-    // s2.add(connect2);
-    // let connect3 = new Connect({ x: 10, y: 350 }, { x: 150, y: 350 });
-    // s2.add(connect3);
-  }
-  ngOnInit(): void {
-
-  }
+  ngAfterViewInit(): void {}
+  ngOnInit(): void {}
   title = 'sparrow-web-server';
 
   logout() {
@@ -97,11 +92,6 @@ export class AppComponent implements AfterViewInit {
   }
 
   onChange(e: any) {
-    console.log(e);
-    sessionStorage.setItem('organizationId', e.value);
-
-    this.rest
-      .create(JSON.stringify([{}]), 'cn.sparrowmini.server.TestCommon')
-      .subscribe((a: any) => {});
+    sessionStorage.setItem('organizationId', JSON.stringify(e.value));
   }
 }
